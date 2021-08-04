651dc6852f05d01f5ed2735a4b99ef8ad45adb27
Nikolaj Zilcov <82801812+K0k0sc0m@users.noreply.github.com>
2021-08-04 22:00:03 +0300

    1+2 uzduotis

--- index.php
+++ index.php
@@ -12,18 +12,52 @@ foreach($arr as $a){
 }
 
 echo "<br>";
+echo "<hr>";
 
+
+//  $arr1 = range(0,9);
+//  print_r($arr1);
 
 $arr1 = [];
 $b = 0;
 
-
-
-for ($i=0; $i < 11 ; $i++) { 
+for ($i=0; $i < 10 ; $i++) { 
     $arr1[$i] = $b++;
 }
 
 print_r($arr1);
+echo "<br>";
+
+
+//  $arr2 = range(10,19);
+//  print_r($arr2);
+
+$arr2 = [];
+$c = 10;
+
+for ($i=0; $i < 10 ; $i++) { 
+    $arr2[$i] = $c++;
+}
+
+print_r($arr2);
+echo "<br>";
+
+for ($i=0; $i < 101; $i++) { 
+       $d = array_rand($arr2);
+
+        echo $arr2[$d]. " ";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
